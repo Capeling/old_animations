7ef850c7a35054e12a1a22801c6a82409bab94f9
Capeling <113705504+Capeling@users.noreply.github.com>
2025-05-21 20:46:59 +0930

    Old Achievement

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/AchievementBar.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(AchievementBar) {
+	bool init(char const* title, char const* desc, char const* icon, bool quest) {
+		if (!AchievementBar::init(title, desc, icon, quest))
+			return false;
+
+		m_bg->setContentSize({ 300.f, 70.f });
+		m_layerColor->setPositionY(35.f);
+
+		return true;
+	}
+	void show() {
+		auto scene = CCScene::get();
+		auto director = CCDirector::get();
+
+		int highestZ = scene->getHighestChildZ();
+		highestZ++;
+
+		highestZ = std::clamp(highestZ, 1000, 0);
+
+		scene->addChild(this, highestZ);
+		
+		this->setPositionY(director->getScreenTop() + 2.f);
+		m_achievementGlowSprite->setVisible(false);
+
+		auto moveDown = CCEaseInOut::create(CCMoveBy::create(1.f, ccp(.0f, -74)), 2.f);
+		auto delay1P5 = CCDelayTime::create(1.5f);
+
+		auto moveUp = CCEaseInOut::create(CCMoveBy::create(1.f, ccp(.0f, 74)), 2.f);
+		auto callFunc = CCCallFunc::create(AchievementNotifier::sharedState(), callfunc_selector(AchievementNotifier::achievementDisplayFinished));
+
+		auto sequence = CCSequence::create(
+			moveDown,
+			delay1P5,
+			moveUp,
+			callFunc,
+			0
+		);
+		this->runAction(sequence);
+	}
+};
+
+class $modify(MenuLayer) {
+	void keyDown(enumKeyCodes key) {
+		if (key == enumKeyCodes::KEY_G) {
+			AchievementNotifier::sharedState()->notifyAchievement("test ach", "ach", "player_11", false);
+		}
+		MenuLayer::keyDown(key);
+	}
+};
