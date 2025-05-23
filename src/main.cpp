e7bb6306d3a18803cbb2349eb47941228d9d61b7
Capeling <113705504+Capeling@users.noreply.github.com>
2025-05-23 20:35:51 +0930

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,48 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/AchievementBar.hpp>
-#include <Geode/modify/MenuLayer.hpp>
 
-using namespace geode::prelude;
-
-class $modify(AchievementBar) {
+struct HookAchievementBar : geode::Modify<HookAchievementBar, AchievementBar> {
 	bool init(char const* title, char const* desc, char const* icon, bool quest) {
 		if (!AchievementBar::init(title, desc, icon, quest))
 			return false;
-
+	
 		m_bg->setContentSize({ 300.f, 70.f });
 		m_layerColor->setPositionY(35.f);
 
+		m_unkUnused = 74;
+		
+		std::string iconStr = icon;
+		if (iconStr.starts_with("color")) {
+			auto sfc = cocos2d::CCSpriteFrameCache::get();
+			m_achievementDescription->setString("Unlocked new Color!");
+
+			if (auto item = geode::cast::typeinfo_cast<GJItemIcon*>(m_achievementSprite)) {
+				item->m_player->setDisplayFrame(sfc->spriteFrameByName("playerSquare_001.png"));
+			}
+		}
+	
 		return true;
 	}
+	
 	void show() {
-		auto scene = CCScene::get();
-		auto director = CCDirector::get();
-
+		auto scene = cocos2d::CCScene::get();
+		auto director = cocos2d::CCDirector::get();
+		
 		int highestZ = scene->getHighestChildZ();
 		highestZ++;
-
+		
 		scene->addChild(this, highestZ);
 		
 		this->setPositionY(director->getScreenTop() + 2.f);
 		m_achievementGlowSprite->setVisible(false);
-
-		auto moveDown = CCEaseInOut::create(CCMoveBy::create(1.f, ccp(.0f, -74)), 2.f);
-		auto delay1P5 = CCDelayTime::create(1.5f);
-
-		auto moveUp = CCEaseInOut::create(CCMoveBy::create(1.f, ccp(.0f, 74)), 2.f);
-		auto callFunc = CCCallFunc::create(AchievementNotifier::sharedState(), callfunc_selector(AchievementNotifier::achievementDisplayFinished));
-
-		auto sequence = CCSequence::create(
+		
+		auto moveDown = cocos2d::CCEaseInOut::create(cocos2d::CCMoveBy::create(1.f, ccp(.0f, -m_unkUnused)), 2.f);
+		auto delay1P5 = cocos2d::CCDelayTime::create(1.5f);
+		
+		auto moveUp = cocos2d::CCEaseInOut::create(cocos2d::CCMoveBy::create(1.f, ccp(.0f, m_unkUnused)), 2.f);
+		auto callFunc = cocos2d::CCCallFunc::create(AchievementNotifier::sharedState(), callfunc_selector(AchievementNotifier::achievementDisplayFinished));
+		
+		auto sequence = cocos2d::CCSequence::create(
 			moveDown,
 			delay1P5,
 			moveUp,
@@ -44,10 +54,14 @@ class $modify(AchievementBar) {
 };
 
 #ifdef DEBUG_BUILD
+#include <Geode/modify/MenuLayer.hpp>
+
 class $modify(MenuLayer) {
-	void keyDown(enumKeyCodes key) {
-		if (key == enumKeyCodes::KEY_G) {
-			AchievementNotifier::sharedState()->notifyAchievement("long title lada dada dada dada", "ach", "color2_11", false);
+	void keyDown(cocos2d::enumKeyCodes key) {
+		if (key == cocos2d::enumKeyCodes::KEY_G) {
+			AchievementNotifier::sharedState()->notifyAchievement("Quest Complete!", "Collect 200 Mana Orbs", "currencyOrbIcon_001.png", true);
+		} else if (key == cocos2d::enumKeyCodes::KEY_H) {
+			AchievementNotifier::sharedState()->notifyAchievement("Test Achievement", "", "color_11", false);
 		}
 		MenuLayer::keyDown(key);
 	}
